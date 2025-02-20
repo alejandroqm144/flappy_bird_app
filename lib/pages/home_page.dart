6b6bade7deae6363ccf6ff938c38f2889566977e
alejandroqm144 <alejandroqm144@gmail.com>
2025-02-19 23:37:11 -0600

    Animated Container added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 
 
+import 'dart:async';
 import 'package:flappy_bird_app/objects/bird.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  double birdYaxis = -1; // Variable para controlar la posición vertical del pájaro (inicia en -1, arriba)
+
+  void jump() {
+    // Método para simular el salto del pájaro
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
+      // Timer.periodic ejecuta una función cada 100 milisegundos
+      setState(() {
+        birdYaxis += 0.1; // Incrementa la posición en el eje Y (mueve el pájaro hacia abajo)
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // Método build que define la interfaz de usuario de la página
@@ -21,10 +35,15 @@ class _HomePageState extends State<HomePage> {
         Expanded(
           flex: 3, // Se define la proporción de espacio que ocupará este contenedor
           // Expandemos el contenedor azul (para simular el cielo)
-          child: Container(
-            color: Colors.blue, // Color de fondo del contenedor (azul)
-            child: Center(child: MyBird()), // Centra el widget MyBird dentro del contenedor
-        ),
+          child: GestureDetector(
+            onTap: jump,
+            child: AnimatedContainer(
+              alignment: Alignment(0, birdYaxis),
+              duration: Duration(milliseconds: 0),
+              color: Colors.blue, // Color de fondo del contenedor (azul)
+              child: Center(child: MyBird()), // Centra el widget MyBird dentro del contenedor
+                    ),
+          ),
         ),
         Expanded(
           child: Container(
